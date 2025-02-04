b0d18122aa906316abb09d812aeac91c2b30573b
A <A@DESKTOP-HLLFQK6>
2025-02-04 18:11:15 +0900

    day1 test

---
+++ java/src/day1/_01_HelloWorld.java
@@ -0,0 +1,11 @@
+package day1;
+
+public class _01_HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("Hello World");
+		System.out.print("안녕하세요:)");
+	}
+
+}
