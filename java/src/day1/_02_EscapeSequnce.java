f17c8f5ba12ef81b913d471744573e7d5ca1fe82
A <A@DESKTOP-HLLFQK6>
2025-02-04 19:03:41 +0900

    day1

--- java/src/day1/_02_EscapeSequnce.java
+++ java/src/day1/_02_EscapeSequnce.java
@@ -3,11 +3,14 @@ package day1;
 public class _02_EscapeSequnce {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("write once!");
+		// TODO Auto-generated method stub //줄바꿈
+		System.out.println("write once!"); // 탭
 		System.out.println("사과\t배\t오렌지");
 		System.out.println("apple\tpear\torange");
-		System.out.println("우리집 강쥐는 \"멍멍\" 짖어요");
+		System.out.println("우리집 강쥐는 \"멍멍\" 짖어요"); // 큰따음표 문자열처리
+		// 한줄 주석입니다. 
+		
+		
 	}
 
 }
