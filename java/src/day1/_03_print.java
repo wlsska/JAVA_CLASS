6692d6b5a3258a441e8ebe1efa5cf07356c52f87
A <A@DESKTOP-HLLFQK6>
2025-02-06 19:41:27 +0900

    day1

--- java/src/day1/_03_print.java
+++ java/src/day1/_03_print.java
@@ -3,8 +3,25 @@ package day1;
 public class _03_print {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		System.out.println("apple");
+		System.out.println("banana");
+		System.out.println("orange");
+		//문자열을 출력하므로 큰따음표 사용
+		
+		System.out.println("");
+		System.out.println(5);
+		System.out.println(3.14);
+		System.out.println('P');
+		
+		System.out.println("");
+		
+		System.out.println(3+5);
+		System.out.println(3.9+5.72);
+		System.out.println(3.9*5.72);
+		System.out.println(10/3);
+		// 정수 나눗셈의 결과는 몫을 나타낸다. 
+		System.out.println(10/3.0);
+		//피연산증 실수가 하나라도 있으면 결과값은 실수로 출력된다. 
 	}
 
 }
